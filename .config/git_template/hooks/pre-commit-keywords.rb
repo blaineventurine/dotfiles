7b88c8be868ffde489aa5f966d3e6bd4a69e55da
Blaine Venturine <blaineventurine@gmail.com>
2023-09-26 04:13:11 -0500

    add pre-commit hook and rubocop config

---
+++ .config/git_template/hooks/pre-commit-keywords.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+FORBIDDEN.freeze = [
+  /\bconsole\.log\b/,
+  /\bconsole\.debug\b/,
+  /\bbinding\.irb\b/,
+  /\bbinding\.pry\b/
+]
+
+full_diff = `git diff --cached --`.force_encoding('ASCII-8BIT')
+
+full_diff.scan(%r{^\+\+\+ b/(.+)\n@@.*\n([\s\S]*?)(?:^diff|\z)}).each do |file, diff|
+  added = diff.split("\n").select { |x| x.start_with?('+') }.join("\n")
+  next unless FORBIDDEN.any? { |re| added.match(re) }
+
+  puts %(Git hook forbids adding "#{Regexp.last_match(1) || Regexp.last_match(0)}" to #{file})
+  puts 'To commit anyway, use --no-verify'
+  exit 1
+end
