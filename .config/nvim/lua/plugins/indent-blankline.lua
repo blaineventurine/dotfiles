1fd2ac1dec42e8b1d701f8640bba3bdf023daf6b
Blaine Venturine <blaine.venturine@algolia.com>
2023-10-02 20:38:13 -0500

    fix indent-blankline breaking changes

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,11 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
   config = function()
     require('ibl').setup({
-      space_char_blankline = ' ',
-      show_current_context = true,
-      show_current_context_start = true,
+      -- space_char_blankline = ' ',
+      -- show_current_context = true,
+      -- show_current_context_start = true,
     })
   end,
 }
