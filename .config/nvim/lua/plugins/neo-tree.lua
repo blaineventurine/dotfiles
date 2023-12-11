b9afd56fa65169150ce4aa0ec02041b2bc6d122a
Blaine Venturine <blaine.venturine@algolia.com>
2023-12-11 12:25:41 -0600

    add windowpicker

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,6 +5,7 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    's1n7ax/nvim-window-picker',
   },
   cmd = 'Neotree',
   config = function()
